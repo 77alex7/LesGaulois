eaf92e8a3ef2782861d07dea93aeddd0f326175c
Alexis Bruniera <brl4698a@ensfsi.onmicrosoft.com>
2024-09-19 15:28:52 +0200

    fin class gaulois

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -1,46 +1,45 @@
 package personnages;
 
+import javax.management.loading.PrivateClassLoader;
+
 public class Gaulois {
-	public int getForce() {
-		return force;
-	}
-
-	public void setForce(int force) {
-		this.force = force;
-	}
-
-	public int getEffetPotion() {
-		return effetPotion;
-	}
-
-	public Gaulois(String nom, int force) {
-		this.nom = nom;
-		this.force = force;
-	}
-
-	private String nom;
-	private int force;
-	private int effetPotion = 1;
-
-	public String getNom() {
-		return nom;
-	}
-
-	@Override
-	public String toString() {
-		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
-	}
-
-	public static void main(String[] args) {
+		private String nom;
+		private int force;
+		private int effetPotion = 1;
 		
-	}
-	
-	public void parler(String texte) {
-		prendreParole();
-	}
+		public Gaulois(String nom, int force) {
+			this.nom = nom;
+			this.force = force;
+		}
+		
+		public String getNom() {
+			return nom;
+		}
+		public void parler(String texte) {
+			System.out.println(prendreParole()+"<<"+texte+">>");
+		}
 
-	private void prendreParole() {
-		// TODO Auto-generated method stub
-		System.out.println();
-	}
+		private String prendreParole() {
+			return "Le gaulois" + nom+":";
+		}
+		public void frapper(Romain romain) {
+			System.out.println(nom+"envoie un grand coup dans la mâchoire de "
+					+romain.getNom());
+			romain.recevoirCoup(force/3);
+		}
+		@Override
+		public String toString() {
+			return"Gaulois[nom="+nom+",force="+force+",effetPotion="+effetPotion+"]";
+		}
+		public static void main(String[]args) {
+			//TODO créer un main permettant de tester la classe Gaulois
+		}
+			
+		
+		
+		
+		
+		
+	
+	
 }
